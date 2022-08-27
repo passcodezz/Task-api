3428d0f6c9ea2a358f9c9be4ff1f62138e4e6b9a
passcodezz <fsczboyza@gmail.com>
2022-08-28 00:17:50 +0700

    fix Calendar Timers & 201-200

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-server.use(router);
+// server.use(router);
+// server.use(jsonServer.bodyParser);
 
-server.listen(port);
+// server.use((req, res, next) => {
+//     if (
+//       req.path.startsWith("/automation") &&
+//       req.headers["authorization"] !== "Bearer abcd"
+//     ) {
+//       return res.status(401).json({ error: "Must pass bearer token abcd" });
+//     }
+  
+//     next();
+//   });
+  server.use(router);
+  
+  server.listen(port);
+  // json-server --watch db.json --port 8080
+  // server.listen(8080, () => {
+  //   console.log("Running on http://localhost:3000");
+  // });
