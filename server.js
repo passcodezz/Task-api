dbd43c60bc61cf3beefbe0e0b4f0e6e5ab22b4d4
konklom <siwakorn.swkk@gmail.com>
2022-08-29 17:40:42 +0700

    update 1000-1200

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./data/db.json');
+const router = jsonServer.router("./data/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 // server.use(router);
-// server.use(jsonServer.bodyParser);
+server.use(jsonServer.bodyParser);
 
-// server.use((req, res, next) => {
-//     if (
-//       req.path.startsWith("/automation") &&
-//       req.headers["authorization"] !== "Bearer abcd"
-//     ) {
-//       return res.status(401).json({ error: "Must pass bearer token abcd" });
-//     }
-  
-//     next();
-//   });
-  server.use(router);
-  
-  server.listen(port);
-  // json-server --watch db.json --port 8080
-  // server.listen(8080, () => {
-  //   console.log("Running on http://localhost:3000");
-  // });
+server.use((req, res, next) => {
+  if (
+    req.path.startsWith("/automation") &&
+    req.headers["authorization"] !== "Bearer abcd"
+  ) {
+    return res.status(401).json({ error: "Must pass token" });
+  }
+
+  next();
+});
+server.use(router);
+
+server.listen(port);
+// json-server --watch db.json --port 8080
+// server.listen(8080, () => {
+//   console.log("Running on http://localhost:3000");
+// });
